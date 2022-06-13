0182ba284661a766889da4ad2a9d813f7325e5be
joska <jpotin]gmail.com>
2022-06-13 19:53:13 +0200

    adding comments

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,39 @@
+/**
+ * It is a helper function to fake async function
+ *
+ * @param {integer} stallTime - The time in milliseconds for the promise to be resolved.
+ */
 async function stall(stallTime = 1000) {
   await new Promise((resolve) => {
     setTimeout(resolve, stallTime)
   })
 }
 
-export async function getEmployees() {
+/**
+ * It is a function that fetches data from the API.
+ * For the sake of this example, it is a fake API.
+ *
+ * @param {object} abortController - The abort controller to be used to abort the request.
+ * @returns {object[]} - The data fetched from the API.
+ */
+export async function getEmployees(abortController) {
   await stall()
   const employees = JSON.parse(localStorage.getItem("employees"))
   if (employees) {
     return employees
   }
 
-  const response = await fetch("./mock/employees.json")
+  const response = await fetch("./mock/employees.json", { signal: abortController.signal })
   const data = await response.json()
   return data
 }
 
+/**
+ * It is a function that saves data to the API.
+ * For the sake of this example, it is a fake API.
+ *
+ * @param {object} employee
+ */
 export function saveEmployee(employee) {
   const employees = JSON.parse(localStorage.getItem("employees")) || []
   employees.push(employee)
