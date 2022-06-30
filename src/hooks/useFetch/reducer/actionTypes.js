c1eaba1c64dd674d65ca9977249440ed172970ca
joska <jpotin@gmail.com>
2022-06-30 16:49:38 +0200

    remove usefetch hook

--- src/hooks/useFetch/reducer/actionTypes.js
+++
@@ -1,7 +0,0 @@
-const actionTypes = Object.freeze({
-  REQUEST_STARTED: "REQUEST_STARTED",
-  REQUEST_SUCCESSFUL: "REQUEST_SUCCESSFUL",
-  REQUEST_FAILED: "REQUEST_FAILED",
-})
-
-export default actionTypes
