7956819fbe1087afaa46921e311094b618461ba4
joska <jpotin@gmail.com>
2022-08-04 18:09:18 +0200

    Updates

--- assets/selectors.1d54c3b4.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./index.89aa2abf.js";const o={HOME:"/",EMPLOYEES_LIST:"employee-list"},l=e,a=s=>s.employees.status,c=s=>s.employees.list,p=s=>s.modal.isOpen;export{o as R,a,c as b,p as s,l as u};
