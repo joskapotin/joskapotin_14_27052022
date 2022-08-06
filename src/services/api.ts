99710ac147bfba784bc627c9fc67ee3fd85f8cbb
joska <jpotin@gmail.com>
2022-08-06 21:28:18 +0200

    soutenance support

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,9 +6,9 @@ export type Response = { employees: Employee[]; message: string }
 /**
  * Simulate slow async calls
  */
-const stall = async (stallTime = 3000) => {
-  await new Promise(resolve => setTimeout(resolve, stallTime))
-}
+// const stall = async (stallTime = 3000) => {
+//   await new Promise(resolve => setTimeout(resolve, stallTime))
+// }
 
 /**
  * It get employees from local storage or mock data if it's not exist
@@ -18,7 +18,7 @@ const getEmployees = async () => {
   const localEmployees = item ? (JSON.parse(item) as Employee[]) : null
   const employees = localEmployees || mockedEmployees
 
-  await stall(5000)
+  // await stall(5000)
 
   return Promise.resolve(employees)
   // return Promise.reject(new Error('Employees not found'))
@@ -33,7 +33,7 @@ const saveEmployee = async (employee: Employee) => {
   employees.push(employee)
   localStorage.setItem('employees', JSON.stringify(employees))
 
-  await stall(5000)
+  // await stall(5000)
 
   return Promise.resolve({ employees, message: 'Employee was successfully created' })
   // return Promise.reject(new Error('Employee was not created'))
